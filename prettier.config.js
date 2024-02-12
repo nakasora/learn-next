2d2a540e0f817ae8546a079aa96dae086f00cbab
github nakasora <n17302826@gmail.com>
2024-02-12 09:53:47 +0900

    complete:leaen-next chapter2

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,7 @@ const styleguide = require('@vercel/style-guide/prettier');
 module.exports = {
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
 };
